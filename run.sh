2371d858978792f8c68ddb7bcd4ca0b79dcd1b28
zhu_peijun <peijun.zhu@drecom.co.jp>
2016-07-05 11:41:19 +0900

    Add compile option to run script

--- run.sh
+++ run.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-file=$1
+file=${!#}
 ext="${file##*.}"
 out="${file%.*}.o"
 
+run=1
+while getopts :c opt
+do
+  case "$opt" in
+    c)
+      run=0
+      ;;
+    ?)
+      echo "Not supported options." >&2
+      exit 0
+      ;;
+  esac
+done
+
 if [ "$ext" == "go" ]
 then
-  go build -o $out $1
+  go build -o $out $file
 else
-  g++ -std=c++11 $1 -o $out
+  g++ -std=c++11 $file -o $out
 fi
 
 if [ $? -ne 0 ]
@@ -16,8 +30,11 @@ then
   echo "Compile Failed!"
 else
   echo "Compile Successed!"
-  st=$(($(gdate +%s%N)/1000000))
-  ./$out
-  ed=$(($(gdate +%s%N)/1000000))
-  echo "$ed $st" | awk '{printf "Total running time: %.5fs.\n", ($1 - $2) / 1000.0f}'
+  if [ $run -ne 0 ]
+  then
+    st=$(($(gdate +%s%N)/1000000))
+    ./$out
+    ed=$(($(gdate +%s%N)/1000000))
+    echo "$ed $st" | awk '{printf "Total running time: %.5fs.\n", ($1 - $2) / 1000.0f}'
+  fi
 fi
